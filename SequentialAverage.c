362b2402c9c553e86ed5399a5bc6f74afac974f8
Jake Shepherd <jakeshepherd98@gmail.com>
2020-10-25 18:01:45 +0000

    Use temp array properly

--- SequentialAverage.c
+++ SequentialAverage.c
@@ -22,36 +22,47 @@ double *populateMainArray(int dimension) {
     return arr;
 }
 
-double *populateTempArray(double *arr, int dimension) {
-    double *tempArray = (double *) malloc((unsigned) (dimension * dimension) * sizeof(double));
-
+void populateTempArray(double *arr, double *tempArray, int dimension) {
     for (int i = 0; i < dimension; i++) {
         for (int j = 0; j < dimension; j++) {
-            tempArray[dimension * i * j] = arr[dimension * i + j];
+            tempArray[dimension * i + j] = arr[dimension * i + j];
         }
     }
-
-    return tempArray;
 }
 
-double *relaxationMethod(double *arr, int dimension) {
+void printArray(double *arr, int dimension) {
+    for (int i = 0; i < dimension; i++) {
+        for (int j = 0; j < dimension; j++) {
+            printf("%f, ", arr[dimension * i + j]);
+        }
+        printf("\n");
+    }
+}
+
+void relaxationMethod(double *arr, int dimension) {
     int loopCount = 0;
-    double accuracy = 0.01, average;
-    bool continueFlag = true;
+    double accuracy = 0.01;
+    bool precisionNotReached = true;
+    double *tempArray = malloc((unsigned long) dimension * (unsigned long) dimension * sizeof(double));
 
-    // take a copy of the original array so that all the values for the average are the original
-    double *tempArray = populateTempArray(arr, dimension);
+    printf("---------BEFORE AVERAGE---------\n");
+    printArray(arr, dimension);
 
-    while (continueFlag == true) {
-        continueFlag = false;
+    while (precisionNotReached) {
+        precisionNotReached = false;
+        loopCount++;
+
+        // take a copy of the original array so that all the values for the average are the original
+        populateTempArray(arr, tempArray, dimension);
+
         // go through array and calc average for center of block
         for (int i = 1; i < dimension - 1; i++) {
             for (int j = 1; j < dimension - 1; j++) {
-                average = calculateMean(
-                        arr[dimension * (i - 1) + j],
-                        arr[dimension * (i + 1) + j],
-                        arr[dimension * i + (j + 1)],
-                        arr[dimension * i + (j - 1)]
+                double average = calculateMean(
+                        tempArray[dimension * (i - 1) + j],
+                        tempArray[dimension * (i + 1) + j],
+                        tempArray[dimension * i + (j + 1)],
+                        tempArray[dimension * i + (j - 1)]
                 );
 
                 /**
@@ -59,27 +70,19 @@ double *relaxationMethod(double *arr, int dimension) {
                  * and replace old center value with the new average
                  */
                 if (fabs(tempArray[dimension * i + j] - average) > accuracy) {
-                    tempArray[dimension * i + j] = arr[dimension * i + j];
                     arr[dimension * i + j] = average;
-                    continueFlag = true;
+                    precisionNotReached = true;
                 }
             }
         }
-        loopCount++;
     }
 
     printf("LOOP COUNT: %d \n", loopCount);
-
     printf("---------AFTER AVERAGE---------\n");
-    for (int i = 0; i < dimension; i++) {
-        for (int j = 0; j < dimension; j++) {
-            printf("%f, ", arr[dimension * i + j]);
-        }
-        printf("\n");
-    }
+    printArray(arr, dimension);
 
     free(tempArray);
-    return arr;
+//    return arr;
 }
 
 int main(int argc, char **argv) {
@@ -101,7 +104,6 @@ int main(int argc, char **argv) {
 //            1.138607, 2.561096, 0.339483, 2.686907, 2.853677, 0.757116,
 //            1.847040, 2.198303, 1.881920, 0.435748, 0.622504, 1.426581
 //    };
-
 
     // start the clock for the averaging off the array
     clock_t begin = clock();
